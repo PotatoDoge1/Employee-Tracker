18a8ac5c1f958cca71488bb809e8fdfef103a237
PotatoDoge1 <ryan.a.schwark@gmail.com>
2024-10-23 21:57:14 -0400

    refactoring and adding basic queries

---
+++ src/sqlClasses/addDepartment.ts
@@ -0,0 +1,23 @@
+class addDepartment {
+    private depName: string;
+
+    constructor(depName: string) {
+        this.depName = depName;
+    }
+
+    async buildQuery(): Promise <string> {
+        try{
+            // Start the transaction
+            await client.query('BEGIN');
+            // Insert the department
+            await client.query(`
+                INSERT INTO department (id, name)
+                VALUES ()`)
+
+        } catch (err) {
+            console.error('Error building query in addDepartment.ts:', err);
+            return err;
+        }
+    }
+
+}
