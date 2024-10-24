8251e89836ec7fd1183bfd45ebf7722eb8939710
PotatoDoge1 <ryan.a.schwark@gmail.com>
2024-10-23 22:02:45 -0400

    forgot to comment out addDepartments.ts file

--- src/sqlClasses/addDepartment.ts
+++ src/sqlClasses/addDepartment.ts
@@ -1,23 +1,23 @@
-class addDepartment {
-    private depName: string;
+// class addDepartment {
+//     private depName: string;
 
-    constructor(depName: string) {
-        this.depName = depName;
-    }
+//     constructor(depName: string) {
+//         this.depName = depName;
+//     }
 
-    async buildQuery(): Promise <string> {
-        try{
-            // Start the transaction
-            await client.query('BEGIN');
-            // Insert the department
-            await client.query(`
-                INSERT INTO department (id, name)
-                VALUES ()`)
+//     async buildQuery(): Promise <string> {
+//         try{
+//             // Start the transaction
+//             await client.query('BEGIN');
+//             // Insert the department
+//             await client.query(`
+//                 INSERT INTO department (id, name)
+//                 VALUES ()`)
 
-        } catch (err) {
-            console.error('Error building query in addDepartment.ts:', err);
-            return err;
-        }
-    }
+//         } catch (err) {
+//             console.error('Error building query in addDepartment.ts:', err);
+//             return err;
+//         }
+//     }
 
-}
+// }
